f9c6105774fc7ca55fcd8ca6955b50e5c920f634
KimiMitsuji <KennyMHT7@hotmail.com>
2023-01-16 19:27:23 -0300

    Criação dos atributos e metodos em Produto

--- src/main/java/br/edu/poo/back/Produto.java
+++ src/main/java/br/edu/poo/back/Produto.java
@@ -2,6 +2,50 @@ package br.edu.poo.back;
 
 public class Produto implements IProdutos{
 
+    private int codigo;
+    private String nome;
+    private double preco;
+    private double quantidade;
+
+    public Produto(int codigo, String nome, double preco, double quantidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+
     @Override
     public void addProduto(Produto p) throws Exception {
         // TODO Auto-generated method stub
