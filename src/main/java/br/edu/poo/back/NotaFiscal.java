fadaf57db623b6e686647df56039cdc6952c108a
KimiMitsuji <KennyMHT7@hotmail.com>
2023-01-16 19:20:04 -0300

    adição da impletação de InotasFicais em Nota Fical

--- src/main/java/br/edu/poo/back/NotaFiscal.java
+++ src/main/java/br/edu/poo/back/NotaFiscal.java
@@ -1,5 +1,41 @@
 package br.edu.poo.back;
 
-public class NotaFiscal {
+public class NotaFiscal implements INotasFiscais {
+
+    @Override
+    public void addNotaFiscal(NotaFiscal nf) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void removeNotaFiscal(int codigo) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public NotaFiscal getNotaFiscal(int codigo) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public double getTotal(int codigo) throws Exception {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void addItem(int codigo, Item item) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void removeItem(int codigo, Item item) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
