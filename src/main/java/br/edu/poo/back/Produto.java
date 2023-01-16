db48049271575b78d730ea8e3610dd49a3365a02
KimiMitsuji <KennyMHT7@hotmail.com>
2023-01-16 19:16:27 -0300

    Produto implementa Iprodutos

--- src/main/java/br/edu/poo/back/Produto.java
+++ src/main/java/br/edu/poo/back/Produto.java
@@ -1,5 +1,47 @@
 package br.edu.poo.back;
 
-public class Produto {
+public class Produto implements IProdutos{
+
+    @Override
+    public void addProduto(Produto p) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void removeProduto(int codigo) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Produto getProduto(int codigo) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void updateQuantidade(int codigo, double nova) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void updatePreco(int codigo, double novo) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addQuantidade(int codigo, double quantidade) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void subQuantidade(int codigo, double quantidade) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
