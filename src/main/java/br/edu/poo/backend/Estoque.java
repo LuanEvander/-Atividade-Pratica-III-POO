fd8faca0213819d9424017ee4dce6a05fca863ae
LuanEvander <luanevander.dev@gmail.com>
2023-01-22 19:15:12 -0300

    Alteração boas praticas tratamento de exceção

--- src/main/java/br/edu/poo/backend/Estoque.java
+++ src/main/java/br/edu/poo/backend/Estoque.java
@@ -1,5 +1,6 @@
 package br.edu.poo.backend;
 
+import br.edu.poo.backend.Exceptions.*;
 import java.util.ArrayList;
 
 public class Estoque implements IProdutos {
@@ -7,115 +8,105 @@ public class Estoque implements IProdutos {
     private static ArrayList<Produto> listaProdutos = new ArrayList<Produto>();
 
     @Override
-    public void addProduto(Produto p) throws Exception {
-        if (p != null && p instanceof Produto && !existe(p.getCodigo())) {
-            listaProdutos.add(p);
-        } else {
-            throw new Exception("Produto inválido");
+    public void addProduto(Produto p) throws ProdutoInvalidoException, CodigoInvalidoException {
+        if (p == null) {
+            throw new ProdutoInvalidoException("Produto não pode ser nulo");
+        }
+        if (!(p instanceof Produto)) {
+            throw new ProdutoInvalidoException("Objeto não é do tipo Produto");
+        }
+        if (existe(p.getCodigo())) {
+            throw new ProdutoInvalidoException("Produto com esse código já existe");
+        }
+        listaProdutos.add(p);
+    }
+
+    @Override
+    public void removeProduto(int codigo) throws CodigoInvalidoException {
+        if (!existe(codigo)) {
+            throw new CodigoInvalidoException("Código inválido");
+        }
+        for (Produto produto : listaProdutos) {
+            if (produto.getCodigo() == codigo) {
+                listaProdutos.remove(produto);
+                break;
+            }
         }
     }
 
     @Override
-    public void removeProduto(int codigo) throws Exception {
-        if (existe(codigo)) {
-            for (Produto produto : listaProdutos) {
-                if (produto.getCodigo() == codigo) {
-                    listaProdutos.remove(produto);
-                    break;
-                }
-            }
-        } else {
-            throw new Exception("Código inválido");
+    public Produto getProduto(int codigo) throws CodigoInvalidoException {
+        if (!existe(codigo)) {
+            throw new CodigoInvalidoException("Código inválido");
         }
-
-    }
-
-    @Override
-    public Produto getProduto(int codigo) throws Exception {
-        if (existe(codigo)) {
-            for (Produto produto : listaProdutos) {
-                if (produto.getCodigo() == codigo) {
-                    return produto;
-                }
+        for (Produto produto : listaProdutos) {
+            if (produto.getCodigo() == codigo) {
+                return produto;
             }
-        } else {
-            throw new Exception("Código inválido");
         }
         return null;
     }
 
     @Override
-    public void updateQuantidade(int codigo, double nova) throws Exception {
-        if (existe(codigo)) {
-            for (Produto produto : listaProdutos) {
-                if (produto.getCodigo() == codigo) {
-                    produto.setQuantidade(nova);
-                    break;
-                }
-            }
-        } else {
-            throw new Exception("Código inválido");
+    public void updateQuantidade(int codigo, double nova) throws CodigoInvalidoException {
+        if (!existe(codigo)) {
+            throw new CodigoInvalidoException("Código inválido");
         }
-
+        for (Produto produto : listaProdutos) {
+            if (produto.getCodigo() == codigo) {
+                produto.setQuantidade(nova);
+                break;
+            }
+        }
     }
 
     @Override
-    public void updatePreco(int codigo, double novo) throws Exception {
-        if (existe(codigo)) {
-            for (Produto produto : listaProdutos) {
-                if (produto.getCodigo() == codigo) {
-                    produto.setPreco(novo);
-                    break;
-                }
-            }
-        } else {
-            throw new Exception("Código inválido");
+    public void updatePreco(int codigo, double novo) throws CodigoInvalidoException {
+        if (!existe(codigo)) {
+            throw new CodigoInvalidoException("Código inválido");
         }
-
-
+        for (Produto produto : listaProdutos) {
+            if (produto.getCodigo() == codigo) {
+                produto.setPreco(novo);
+                break;
+            }
+        }
     }
 
     @Override
-    public void addQuantidade(int codigo, double quantidade) throws Exception {
-        if (existe(codigo)) {
-            for (Produto produto : listaProdutos) {
-                if (produto.getCodigo() == codigo) {
-                    produto.setQuantidade(produto.getQuantidade() + quantidade);
-                    break;
-                }
-            }
-        } else {
-            throw new Exception("Código inválido");
+    public void addQuantidade(int codigo, double quantidade) throws CodigoInvalidoException {
+        if (!existe(codigo)) {
+            throw new CodigoInvalidoException("Código inválido");
         }
-
-
+        for (Produto produto : listaProdutos) {
+            if (produto.getCodigo() == codigo) {
+                produto.setQuantidade(produto.getQuantidade() + quantidade);
+                break;
+            }
+        }
     }
 
     @Override
-    public void subQuantidade(int codigo, double quantidade) throws Exception {
-        if (existe(codigo)) {
-            for (Produto produto : listaProdutos) {
-                if (produto.getCodigo() == codigo) {
-                    produto.setQuantidade(produto.getQuantidade() - quantidade);
-                    break;
-                }
-            }
-        } else {
-            throw new Exception("Código inválido");
+    public void subQuantidade(int codigo, double quantidade) throws CodigoInvalidoException {
+        if (!existe(codigo)) {
+            throw new CodigoInvalidoException("Código inválido");
         }
-
-
+        for (Produto produto : listaProdutos) {
+            if (produto.getCodigo() == codigo) {
+                produto.setQuantidade(produto.getQuantidade() - quantidade);
+                break;
+            }
+        }
     }
 
-    public boolean existe(int codigo) throws Exception {
-        if (codigo >= 0) {
-            for (Produto produto : listaProdutos) {
-                if (produto.getCodigo() == codigo) {
-                    return true;
-                }
+    public boolean existe(int codigo) throws CodigoInvalidoException {
+        if (codigo < 0) {
+            throw new CodigoInvalidoException("Código inválido");
+        }
+        for (Produto produto : listaProdutos) {
+            if (produto.getCodigo() == codigo) {
+                return true;
             }
-        } else {
-            throw new Exception("Código inválido");
         }
         return false;
     }
