a013deab64f6517ad2c2ed432f3d98ffe6accae4
KimiMitsuji <108253678+KimiMitsuji@users.noreply.github.com>
2023-02-06 17:05:28 -0300

    Criação de alertas e planilha

--- src/main/java/br/edu/poo/gui/ControladorEstoque.java
+++ src/main/java/br/edu/poo/gui/ControladorEstoque.java
@@ -1,30 +1,52 @@
 package br.edu.poo.gui;
 
-
 import br.edu.poo.backend.*;
 import br.edu.poo.backend.Exceptions.*;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputControl;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class ControladorEstoque {
     private Estoque metodoEstoque = new Estoque();
 
-    //Tela de Pesquisa
+    // Tela de Pesquisa
     @FXML
-    private TextField PesquisaProduto_txt;
+    private TextField pesquisaProduto_txt;
     @FXML
-    private Button PesquisaProduto_btn;
+    private Button pesquisaProduto_btn;
     @FXML
     private Button addProduto_btn;
-    //Tela de Cadastro
+    @FXML
+    private TableView<Produto> tabelaEstoque;
+    @FXML
+    private TableColumn<Produto, Integer> colunaCodigo;
+    @FXML
+    private TableColumn<Produto, String> colunaNome;
+    @FXML
+    private TableColumn<Produto, String> colunaDescricao;
+    @FXML
+    private TableColumn<Produto, Double> colunaPreco;
+    @FXML
+    private TableColumn<Produto, Double> colunaQuantidade;
+    @FXML
+    private ObservableList<Produto> pesquisaProduto = FXCollections.observableArrayList();
+    // Tela de Cadastro
     @FXML
     private TextField addCodigo_txt;
     @FXML
@@ -37,12 +59,14 @@ public class ControladorEstoque {
     private TextField addQuantidade_txt;
     @FXML
     private Button confirmaCadastro_btn;
-    //Fim da Tela de Cadastro
-    
-    //Tela de Remoção
-    //Fim da Tela de Remoção
+    @FXML
+    private Button cancelar_btn;
+    // Fim da Tela de Cadastro
 
-    //Tela de Atualização
+    // Tela de Remoção
+    // Fim da Tela de Remoção
+
+    // Tela de Atualização
     @FXML
     private TextField attPesquisaProduto_txt;
     @FXML
@@ -53,11 +77,12 @@ public class ControladorEstoque {
     private TextField attQuantidade_txt;
 
     private TextInputControl removerProduto_btn;
-    //Fim da Tela de Atualização
+
+    // Fim da Tela de Atualização
     @FXML
     void cadastrarProduto(ActionEvent event) {
         String nome = addNome_txt.getText();
-        String descricao= addDescricao_txt.getText();
+        String descricao = addDescricao_txt.getText();
 
         int codigo = Integer.parseInt(addCodigo_txt.getText());
         double preco = Double.parseDouble(addPreco_txt.getText());
@@ -67,12 +92,30 @@ public class ControladorEstoque {
 
         try {
             metodoEstoque.addProduto(produto);
+
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Produto Cadastrado");
+            alert.setHeaderText(null);
+            alert.setContentText(produto.toString());
+            alert.showAndWait();
+
         } catch (CodigoInvalidoException e) {
-            e.printStackTrace();
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Erro");
+            alert.setHeaderText(e.getMessage());
+            alert.setContentText("Verifique o código do produto e tente novamente.");
+            alert.showAndWait();
         } catch (ProdutoInvalidoException e) {
-            e.printStackTrace();
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Erro");
+            alert.setHeaderText(e.getMessage());
+            alert.setContentText("Verifique o código do produto e tente novamente.");
+            alert.showAndWait();
         }
+
+        limparCampos(event);
     }
+
 
     void removerProduto() throws Exception {
         String parseCodigo = removerProduto_btn.getText();
@@ -86,21 +129,42 @@ public class ControladorEstoque {
     }
 
     @FXML
-    void obterProduto() throws Exception {
-        String parseCodigo = PesquisaProduto_txt.getText();
-        int codigo = Integer.parseInt(parseCodigo);
+    private void initialize() throws Exception {
+        colunaCodigo.setCellValueFactory(
+                cellData -> new SimpleIntegerProperty(cellData.getValue().getCodigo()).asObject());
+        colunaNome
+                .setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNome()));
+        colunaDescricao.setCellValueFactory(
+                cellData -> new SimpleStringProperty(cellData.getValue().getDescricao()));
+        colunaPreco.setCellValueFactory(
+                cellData -> new SimpleDoubleProperty(cellData.getValue().getPreco()).asObject());
+        colunaQuantidade.setCellValueFactory(
+                cellData -> new SimpleDoubleProperty(cellData.getValue().getQuantidade()).asObject());
+        
+        tabelaEstoque.setItems(pesquisaProduto);
+    }
+
+    @FXML
+    void obterProduto(ActionEvent event) throws Exception {
+        int codigo = Integer.parseInt(pesquisaProduto_txt.getText());
+        Produto produto = null;
 
         try {
-            metodoEstoque.getProduto(codigo);
+            produto = metodoEstoque.getProduto(codigo);
+            pesquisaProduto.add(produto);
         } catch (CodigoInvalidoException e) {
-            e.printStackTrace();
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Erro");
+            alert.setHeaderText(e.getMessage());
+            alert.setContentText("Verifique o código do produto e tente novamente.");
+            alert.showAndWait();
         }
     }
 
     @FXML
     void atualizarQuantidade() throws Exception {
-        String parseCodigo = attPesquisaProduto_id.getText();
-        String parseQuantidade = attQuantidade_id.getText();
+        String parseCodigo = attPesquisaProduto_txt.getText();
+        String parseQuantidade = attQuantidade_txt.getText();
 
         int codigo = Integer.parseInt(parseCodigo);
         double quantidade = Double.parseDouble(parseQuantidade);
@@ -114,7 +178,7 @@ public class ControladorEstoque {
 
     @FXML
     void atualizarPreco() throws Exception {
-        String parseCodigo = PesquisaProduto_txt.getText();
+        String parseCodigo = pesquisaProduto_txt.getText();
         String parsePreco = attPreco_txt.getText();
 
         int codigo = Integer.parseInt(parseCodigo);
@@ -126,6 +190,7 @@ public class ControladorEstoque {
             e.printStackTrace();
         }
     }
+
     @FXML
     private void addProduto(ActionEvent event) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("TelaDeCadastro.fxml"));
@@ -135,6 +200,7 @@ public class ControladorEstoque {
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.show();
     }
+
     @FXML
     private void attProduto(ActionEvent event) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("TelaDeAtualizarProduto.fxml"));
@@ -143,5 +209,20 @@ public class ControladorEstoque {
         stage.setScene(new Scene(root));
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.show();
+    }
+
+    @FXML
+    public void fecharJanela(ActionEvent event) {
+        Stage stage = (Stage) cancelar_btn.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    public void limparCampos(ActionEvent event) {
+        addCodigo_txt.setText("");
+        addNome_txt.setText("");
+        addDescricao_txt.setText("");
+        addPreco_txt.setText("");
+        addQuantidade_txt.setText("");
     }
 }
